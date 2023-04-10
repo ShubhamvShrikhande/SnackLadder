ab79b4155fcaa1d2d94ad15cf2d86ebf17abff0c
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-04-10 17:53:39 +0530

    UC3

--- SnakeLadder/SnakeLadderMain.java
+++ SnakeLadder/SnakeLadderMain.java
@@ -4,34 +4,41 @@ import java.util.Random;
 
 public class SnakeLadderMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder");
-		int StartPos = 0;
-		int Ladder = 1;
-		int Snake = 2;
-		int player_position = 0;
-		System.out.println("Game Starts Now");
-		System.out.println("Starting position of player is: " + StartPos);
-		Random rd = new Random();
-		int diceroll = rd.nextInt(7);// 0-6
-		System.out.println("Player gets Dice Number :: " + diceroll);
-		int n = rd.nextInt(3);
-		System.out.println("case..." + n);
+        System.out.println("Welcome to Snake and Ladder");
+        int StartPos = 0;
+        int Ladder = 1;
+        int Snake = 2;
+        int player_position=0;
+        System.out.println("Game Starts Now");
+        System.out.println("Starting position of player is: "+StartPos);
+        Random rd = new Random();
+        int diceroll = rd.nextInt(7);//0-6
+        System.out.println("Player gets Dice Number :: " + diceroll);
 
-		switch (n) {
-		case 0:
-			System.out.println("No Play");
-			break;
-		case 1:
-			player_position = player_position + diceroll;
-			System.out.println("Ladder : " + player_position);
-			break;
-		case 2:
-			player_position = player_position - diceroll;
-			System.out.println("Snake : " + player_position);
-			break;
-		default:
-			System.out.println("invalid choice");
-		}
-	}
+        while (player_position < 100) {
+            int n = rd.nextInt(3);
+            System.out.println("case..." + n);
 
+            switch (n) {
+                case 0:
+                    System.out.println("No Play");
+                    break;
+                case 1:
+                    player_position = player_position + diceroll;
+                    System.out.println("Ladder : " + player_position);
+                    break;
+                case 2:
+                    player_position = player_position - diceroll;
+                    System.out.println("Snake : " + player_position);
+                    if(player_position<0)
+                    {
+                        player_position = 0;
+                    }
+                    break;
+                default:
+                    System.out.println("invalid choice");
+
+            }
+        }
+    }
 }
